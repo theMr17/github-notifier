e3741299f5c38fbdc4aa9634edb05c74dd7ea545
Saptak Manna <saptakmanna100@gmail.com>
2025-04-06 19:09:31 +0530

    Revert failing tests

--- app/src/androidTest/java/com/notifier/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/notifier/app/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.notifier.app1", appContext.packageName)
+        assertEquals("com.notifier.app", appContext.packageName)
     }
 }
