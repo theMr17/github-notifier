44b202449b41488a9f3b2bc8b13ef78989699203
Saptak Manna <saptakmanna100@gmail.com>
2025-06-09 09:13:10 +0530

    Add loader on NotificationScreen

--- app/src/main/java/com/notifier/app/notification/presentation/NotificationScreen.kt
+++ app/src/main/java/com/notifier/app/notification/presentation/NotificationScreen.kt
@@ -1,8 +1,10 @@
 package com.notifier.app.notification.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,6 +19,16 @@ fun NotificationScreen(
     state: NotificationState,
     modifier: Modifier = Modifier,
 ) {
+    if (state.isLoading) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+        return
+    }
+
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
